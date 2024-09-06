6c3d10cbd8d7fcaade784542dc7c5890b8a035f5
Valeriia Rohatynska <valeriiarohatynska@gmail.com>
2024-09-06 12:49:46 +0200

    Improve useLocalStorage hook to be more reusable

--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -1,12 +1,19 @@
 import { useEffect, useState } from "react";
 
 export function useLocalStorage(key, defaultValue) {
-  const [value, setValue] = useState(
-    JSON.parse(localStorage.getItem(key)) || defaultValue
-  );
+  const [value, setValue] = useState(() => {
+    try {
+      const storedValue = JSON.parse(localStorage.getItem(key));
+      return storedValue ?? defaultValue;
+    } catch (error) {
+      console.error("Error parsing localStorage data:", error);
+      return defaultValue;
+    }
+  });
 
   useEffect(() => {
-    if (value.length > 0) localStorage.setItem(key, JSON.stringify(value));
+    if (value && value.length !== 0)
+      localStorage.setItem(key, JSON.stringify(value));
     else localStorage.removeItem(key);
   }, [key, value]);
 
